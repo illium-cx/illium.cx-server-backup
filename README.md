b94dcbcd70b29abc4275864e487ef96fa63326c8
Bumzag <illium.cx@gmail.com>
2024-12-15 17:09:24 +0000

    old

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# illium.cx-server-backup
-Bash script to backup FiveM server files
+# FiveM Server Backup
+
+For more information on configuring the backup script, check out [this write-up](https://fivem.illium.cx/fivem/automated-fivem-server-backup).
+
+### Outline
+
+- You must have `rw` permission in the `$SERVER_PATH`, since the script
+   creates the `backups` directory in it. It doesn't matter where the script is run. 
+- Once you start the script, the various paths are set based on `$SERVER_PATH`. 
+- Checks/outputs the server's daemon status.
+- Any missing folders are then created, i.e `backups`, `archive`, etc.
+- The previous backup **log** file is moved to `backups/archive/logs`, and a new one is created
+- The previous **backup** file is moved to `backups/archive`
+- If the server daemon is running, it's stopped
+- A new timestamped backup file is created in `backups`
+- The server is restarted
+
+### Usage
+
+Clone the repo, preferably to the server folder, e.g `/opt/fivem-server`:
+```
+cd /opt/fivem-server
+git clone https://github.com/illium-cx/illium.cx-server-backup.git backups/
+```
+
+and then edit the following in the script:
+
+- `$SERVER_PATH`
+
+   Path to the server files, e.g `/opt/fivem-server`
+
+- `$SERVICE_NAME`
+
+   The name of the server's background service
+
+- `$ARCHIVE_LIMIT` - default: `100`
+
+   If the number of backup files in `backup/archive`
+   is > this value, the oldest backup is deleted
+
+`nano backups/backup.sh`
+```
+# backup.sh
+
+$SERVER_PATH="/path/to/your/server/files"
+$SERVICE_NAME="fivem-server.service"
+$ARCHIVE_LIMIT=100
+```
+
+At this point, you can manually run the script whenever you prefer. 
+
+### Automate
+
+
+To automate the process, create a cronjob to run the script every hour. Start by making the script executable
+```
+chmod +x backup.sh
+```
+
+And finally, add an entry to the crontab file. This will run `backup.sh` at the 0 minute of every hour:
+`crontab -e`
+
+```
+# Add this to the bottom of the file
+0 * * * * /opt/fivem-server/backup.sh
+```
+
---
+++ backup.sh
@@ -0,0 +1,89 @@
+#    __     __         __         __     __  __     __    __
+#   /\ \   /\ \       /\ \       /\ \   /\ \/\ \   /\ "-./  \
+#   \ \ \  \ \ \____  \ \ \____  \ \ \  \ \ \_\ \  \ \ \-./\ \
+#    \ \_\  \ \_____\  \ \_____\  \ \_\  \ \_____\  \ \_\ \ \_\
+#     \/_/   \/_____/   \/_____/   \/_/   \/_____/   \/_/  \/_/
+#                                     powered by FiveM & QBCore
+#
+#                                       illium.cx-server-backup
+
+TIMESTAMP="$(date +'%Y-%m-%d_%H-%M-%S')"
+
+# SET BEFORE RUNNING
+SERVER_PATH="/opt/fivem-server"
+SERVICE_NAME="fivem-server.service"
+
+TXDATA_DIR="$SERVER_PATH/txData"
+BACKUP_DIR="$SERVER_PATH/backups"
+BACKUP_ARCHIVE="$SERVER_PATH/backups/archive"
+ARCHIVE_LIMIT=100
+
+LOG_FILE="$SERVER_PATH/backups/$(date +'%Y-%m-%d-%H-%M-%S')-backup.log"
+LOG_ARCHIVE="$SERVER_PATH/backups/archive/logs"
+
+exit_script() {
+    local message="$1"
+    echo "// $TIMESTAMP - $message - EXITING"
+    echo "// $TIMESTAMP - $message - EXITING" >> "$LOG_FILE"
+    exit "${2:-1}"
+}
+
+log() {
+    local message="$1"
+    echo "// $TIMESTAMP - $message"
+    echo "// $TIMESTAMP - $message" >> "$LOG_FILE"
+}
+
+# // ----------------------------------
+# // DIR CHECK
+[[ ! -d "$TXDATA_DIR" ]] && echo "ERROR: No server files found - EXITING" && exit 1
+
+for dir in "$BACKUP_DIR" "$BACKUP_ARCHIVE" "$LOG_ARCHIVE"; do
+    [[ ! -d "$dir" ]] && { echo "$TIMESTAMP - WARNING: $dir does not exist, creating: $dir"; mkdir "$dir" || exit_script "ERROR: Failed to create $dir"; }
+done
+
+# // ----------------------------------
+# // FILE CHECK
+shopt -s nullglob
+[[ -n "$(find "$BACKUP_DIR" -maxdepth 1 -name '*.log' -print -quit)" ]] && mv "$BACKUP_DIR"/*.log "$LOG_ARCHIVE"
+[[ -n "$BACKUP_DIR"/*.log ]] && touch "$LOG_FILE" || exit_script "ERROR: Failed to create log file: $LOG_FILE"
+
+log "FIVEM SERVER BACKUP"
+log "----------------------------------"
+
+# If there's already a backup in $BACKUP_DIR, move it to archive
+backup=("$BACKUP_DIR"/*.tar.gz)
+[[ ${#backup[@]} -gt 0 ]] && mv "${backup[@]}" "$BACKUP_ARCHIVE"
+
+# If archive contains >$ARCHIVE_LIMIT, delete oldest backup
+backup_archive=("$BACKUP_ARCHIVE"/*.tar.gz)
+log "Archive backup count: ${#backup_archive[@]}"
+if [[ ${#backup_archive[@]} -ge $ARCHIVE_LIMIT ]]; then
+    backup_oldest=$(ls -t "$BACKUP_ARCHIVE"/*.tar.gz | tail -n 1)
+    rm -f "$backup_oldest" && log "Removed oldest backup"
+fi
+
+log "$SERVICE_NAME is currently: $([[ "$SERVICE_STATUS" == "ACTIVE" ]] && echo -e "\033[0;32mACTIVE\033[0m" || echo -e "\033[0;31mNOT ACTIVE\033[0m")"
+
+if [[ "$SERVICE_STATUS" == "ACTIVE" ]]; then 
+    if [[ $1 == "-S" ]]; then
+        log "Server shutdown enabled"
+        log "Shutting down $SERVICE_NAME"
+        sudo systemctl stop "$SERVICE_NAME" && log "Successfully stopped $SERVICE_NAME" || { exit_script "ERROR: Failed to stop $SERVICE_NAME"; } 
+    fi
+fi
+
+shopt -u nullglob
+
+cd $SERVER_PATH
+log "SERVER ARCHIVE - START"
+log "----------------------------------"
+tar -cvzf "backups/$TIMESTAMP-backup.tar.gz" "txData" | while read line; do
+	log "$line"
+done
+log "----------------------------------"
+log "SERVER ARCHIVE - FINISH "
+[[ -f "$BACKUP_DIR/$TIMESTAMP-backup.tar.gz" ]] && log "Backup created: $BACKUP_DIR/$TIMESTAMP-backup.tar.gz"
+[[ $1 == "-S" ]] && sudo systemctl start $SERVICE_NAME && log "$SERVICE_NAME started successfully"
+
+exit_script "FINISHED"
